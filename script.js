ff8e0d47a45c9d5b614f0ea3195f5319daeefa4b
SkyFrost111 <tasoskontovrakis@gmail.com>
2025-07-19 21:20:16 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".btn");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+      btn.style.boxShadow = "0 0 15px #e60012";
+    });
+
+    btn.addEventListener("mouseleave", () => {
+      btn.style.boxShadow = "none";
+    });
+  });
+});
